5e948841640e2eed45e4a6cdf32786bf1ebf4b90
Eliot Sanford <eliot587@gmail.com>
2020-08-30 21:51:16 -0500

    Build a working Footer and added fragments into return

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,22 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Hello = (props) => {
+ return( <div>
+  <p>
+    Hey {props.name}, you are {props.age} years old!
+  </p>
+</div>
+)
+}
+
+const Footer = () => {
   return (
-    <div>
-      <p>Hey {props.name}!</p>
-    </div>
+    <>
+      <p>Hey! Hey! app created by <a href="https://github.com/techieeliot">Eliot Sanford</a></p>
+    </>
   )
 }
 
 const App = () => {
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Jude"/>
-      <Hello name="Selah"/>
-      <Hello name="Norah"/>
-    </div>
-  )
+const name = 'Jude'
+const age = 1.5
+
+return (
+<>
+  <h1>Hey! Hey!</h1>
+  <Hello name="Selah" age={1 + 4} />
+  <Hello name={name} age={age} />
+  <Footer />
+</>
+)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
