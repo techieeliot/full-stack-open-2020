ed35d73f2b29c6a119c37b81ca88826a95935382
Eliot Sanford <eliot587@gmail.com>
2020-09-05 17:17:44 -0500

    Refactor counter app to have smaller components - Build a thumbs down component - refactor minus to not go below zero

--- part1/counter/src/index.js
+++ part1/counter/src/index.js
@@ -2,6 +2,44 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const DisplayCounter = ({counter}) => (
+    <h1>{counter}</h1>
+  )
+
+const Button = ({buttonType, handleClick, text}) => (
+    <button className={buttonType} onClick={handleClick}>
+      {text}
+    </button>
+)
+
+const DisplayButtons = ({counter, setCounter}) => {
+  
+  const addOne = () => {setCounter(counter + 1)}
+  const minusOne = () => {
+    setCounter(counter - 1)
+    if(counter<1){
+      return setCounter(0)
+    }
+  }
+  const resetCounter = () => setCounter(0)
+  return(
+      <section>
+        <Button 
+          buttonType="btn" 
+          handleClick={addOne}
+          text="👍"/>
+        <Button 
+          buttonType="btn" 
+          handleClick={minusOne}
+          text="👎"/>
+        <Button 
+          buttonType="btn alt-btn" 
+          handleClick={resetCounter}
+          text="Reset"/>
+      </section>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
   
@@ -10,19 +48,12 @@ const App = () => {
   // 1000
   // )
 
-  const handleClick = () => {setCounter(counter + 1)}
-
   console.log('rendering...', counter)
 
   return (
     <main className="container">
-      <h1>{counter}</h1>
-      <button className="btn" onClick={handleClick}>
-        👍
-      </button>
-      <button className="btn alt-btn" onClick={() => setCounter(0)}> 
-        Reset
-      </button>
+      <DisplayCounter counter={counter} />
+      <DisplayButtons counter={counter} setCounter={setCounter}/>
     </main>
   )
 }
