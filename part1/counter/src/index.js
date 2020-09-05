d46d9d5940796155d2b805964fdcb39e03b19199
Eliot Sanford <eliot587@gmail.com>
2020-09-05 16:05:19 -0500

    Add button with event handler to the console - add styling to the button

--- part1/counter/src/index.js
+++ part1/counter/src/index.js
@@ -5,15 +5,25 @@ import './index.css';
 const App = () => {
   const [counter, setCounter] = useState(0)
   
-  setTimeout(
-    () => setCounter(counter + 1),
-  1000
-  )
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  // 1000
+  // )
+
+  const handleClick = () => {
+    console.log('clicked');
+    
+  }
+
+  console.log('rendering...', counter)
 
   return (
-    <>
+    <main className="container">
       <h1>{counter}</h1>
-    </>
+      <button className="btn" onClick={handleClick}>
+        👍
+      </button>
+    </main>
   )
 }
 
