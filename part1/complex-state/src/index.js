1c0fc9560dcab0a5f65d7d8d9e03cb628788f712
Eliot Sanford <eliot587@gmail.com>
2020-09-06 10:27:06 -0500

    Consolidate the right and left click into one state object

--- part1/complex-state/src/index.js
+++ part1/complex-state/src/index.js
@@ -3,20 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const App = (props) => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = { 
+      left: clicks.left + 1, 
+      right: clicks.right 
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = { 
+      left: clicks.left, 
+      right: clicks.right + 1 
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <main className="container">
       <section className="button-box">
-        {left}
-        <button onClick={() => setLeft(left + 1)}>
-          left
-        </button>
-        <button onClick={() => setRight(right + 1)}>
-          right
-        </button>
-        {right}
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
       </section>
     </main>
   )
