5c7aa5e770310197d4f7b7b8e73379547a9f9b58
Eliot Sanford <eliot587@gmail.com>
2020-09-05 23:45:52 -0500

    Build out new complex state component

---
+++ part1/complex-state/src/index.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  return (
+    <div>
+      <div>
+        {left}
+        <button onClick={() => setLeft(left + 1)}>
+          left
+        </button>
+        <button onClick={() => setRight(right + 1)}>
+          right
+        </button>
+        {right}
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
