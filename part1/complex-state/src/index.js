32cd43bdcb1efd353b96bb0e2f412e8b067da908
Eliot Sanford <eliot587@gmail.com>
2020-09-12 16:51:10 -0500

    Refactor to create store and container for all L and R clicks

--- part1/complex-state/src/index.js
+++ part1/complex-state/src/index.js
@@ -3,32 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const App = (props) => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
 
   const handleLeftClick = () => {
-    setClicks({
-      ...clicks,
-      left: clicks.left + 1 
-    })
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
 
   const handleRightClick = () => {
-    setClicks({
-      ...clicks,
-      right: clicks.right + 1 
-    })
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
     <main className="container">
       <section className="button-box">
-        {clicks.left}
+        {left}
         <button onClick={handleLeftClick}>left</button>
         <button onClick={handleRightClick}>right</button>
-        {clicks.right}
+        {right}
       </section>
+        <p>{allClicks.join(' ')}</p>
     </main>
   )
 }
