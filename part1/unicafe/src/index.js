04a760bf05163ec123b6787cfc1e9e2e517bcaef
Eliot Sanford <eliot587@gmail.com>
2020-09-13 15:36:56 -0500

    Refactor statistics component to display votes and percentage

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,25 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+  if(props.allFeedback === 0){
+    return (
+      <p>Begin using the app by pressing one of the buttons above.</p>
+    )
+  }
+  return(
+    <div>
+      <h1>statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.allFeedback}</p>
+      <p>average {props.average}</p>
+      <p>positive {props.positive}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -8,53 +27,37 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [allFeedback, setFeedback] = useState([])
 
-  const Statistics = (props) => {
-    if(props.allFeedback === 0){
-      return (
-        <p>Begin using the app by pressing one of the buttons above.</p>
-      )
-    }
-    return(
-      <div>
-        <p>all {props.allFeedback}</p>
-        <p>average {props.average}</p>
-        <p>positive {props.positive}</p>
-      </div>
-    )
-  }
-
-  const positive = `${good/allFeedback.length} %`
+  const positive = `${(good/allFeedback.length) * 100} %`
 
   const getSum = (total, num) => total + num
   const average = allFeedback.reduce(getSum, 0) / allFeedback.length
 
   const handleGood = () => {
-      setFeedback(allFeedback.concat(1))
-      setGood(good + 1)
-    }
+    setFeedback(allFeedback.concat(1))
+    setGood(good + 1)
+  }
   const handleNeutral = () => {
-      setFeedback(allFeedback.concat(0))
-      setNeutral(neutral + 1)
-    }
+    setFeedback(allFeedback.concat(0))
+    setNeutral(neutral + 1)
+  }
   const handleBad = () => {
-      setFeedback(allFeedback.concat(-1))
-      setBad(bad + 1)
-    }
+    setFeedback(allFeedback.concat(-1))
+    setBad(bad + 1)
+  }
   
 
   return (
     <>
       <h1>give feedback</h1>
       <section className="buttons">
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
+        <button onClick={handleGood}>good</button>
+        <button onClick={handleNeutral}>neutral</button>
+        <button onClick={handleBad}>bad</button>
       </section>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <Statistics 
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
         allFeedback={allFeedback.length} 
         average={average}
         positive={positive}/>
