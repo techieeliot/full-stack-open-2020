3f6d38c77dacf191a4670360c68877cdb8df5ae1
Eliot Sanford <eliot587@gmail.com>
2020-09-13 14:57:13 -0500

    Add a feedbacklog for totals, average, and positive for 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,19 +6,58 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allFeedback, setFeedback] = useState([])
+
+  const FeedbackLog = (props) => {
+    if(props.allFeedback === 0){
+      return (
+        <p>Begin using the app by pressing one of the buttons above.</p>
+      )
+    }
+    return(
+      <div>
+        <p>all {props.allFeedback}</p>
+        <p>average {props.average}</p>
+        <p>positive {props.positive}</p>
+      </div>
+    )
+  }
+
+  const positive = `${good/allFeedback.length} %`
+
+  const getSum = (total, num) => total + num
+  const average = allFeedback.reduce(getSum, 0) / allFeedback.length
+
+  const handleGood = () => {
+      setFeedback(allFeedback.concat(1))
+      setGood(good + 1)
+    }
+  const handleNeutral = () => {
+      setFeedback(allFeedback.concat(0))
+      setNeutral(neutral + 1)
+    }
+  const handleBad = () => {
+      setFeedback(allFeedback.concat(-1))
+      setBad(bad + 1)
+    }
+  
 
   return (
     <>
       <h1>give feedback</h1>
       <section className="buttons">
-      <button onClick={()=> setGood(good + 1)}>good</button>
-      <button onClick={()=> setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={()=> setBad(bad + 1)}>bad</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
       </section>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <FeedbackLog 
+        allFeedback={allFeedback.length} 
+        average={average}
+        positive={positive}/>
     </>
   )
 }
