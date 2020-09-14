8e2cd1a2177cd07b6ce95c2cd93b12e60ea3ae95
Eliot Sanford <eliot587@gmail.com>
2020-09-14 15:01:06 -0500

    Build out PopularAnecdots and Buttons Components for 1.14

--- part1/anecdote/src/index.js
+++ part1/anecdote/src/index.js
@@ -1,19 +1,48 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const SelectedAnecdote = ({anecdotes, selected}) => (
-  <p>{anecdotes[selected]}</p>
+const Buttons = ({handleNext, handleVote}) => (
+      <section>
+        <button onClick={handleVote}>vote</button>
+        <button onClick={handleNext}>next anecdote</button>
+      </section>
+)
+const PopularAnecdotes = ({anecdote, votes}) => (
+  <>
+    <h1>Anecdote with most votes</h1>
+    <section>
+      <p>{anecdote}</p>
+      <p>has {votes} votes</p>
+    </section>
+  </>
+)
+
+
+const SelectedAnecdote = ({anecdotes, selected, points}) => (
+  <>
+    <h1>Anecdote of the day</h1>
+    <p>{anecdotes[selected]}</p>
+    <p>has {points[selected]} votes</p>
+  </>
 )
 
 const App = () => {
   const [selected, setSelected] = useState(0)
   const initialPoints = new Array(anecdotes.length).fill(0)
   const [points, setPoints] = useState(initialPoints)
-
+  const [popular, setPopular] = useState('If it hurts, do it more often')
+  const [popularPoints, setPopularPoints] = useState(0)
+  
+  const max = Math.max(...points);
+    
   const handleVote = () => {  
     const copy = [...points]
-    copy[selected] += 1
+    copy[selected]++
     setPoints(copy)
+    const pointsLeader = Math.max(...copy)
+    setPopularPoints(pointsLeader)
+    const leaderQuote = anecdotes[copy.indexOf(Math.max(...copy))]
+    setPopular(leaderQuote)
   }
 
   const getRandomNumber = (max) => Math.floor(Math.random() * Math.floor(max))
@@ -23,12 +52,9 @@ const App = () => {
   }
   return (
     <>
-      <SelectedAnecdote anecdotes={anecdotes} selected={selected} />
-      <p>has {points[selected]} votes</p>
-      <section>
-        <button onClick={handleVote}>vote</button>
-        <button onClick={handleNext}>next anecdote</button>
-      </section>
+      <SelectedAnecdote anecdotes={anecdotes} selected={selected} points={points} />
+      <Buttons handleVote={handleVote} handleNext={handleNext} />
+      <PopularAnecdotes anecdote={popular} votes={popularPoints}/>
     </>
   )
 }
