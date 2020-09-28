a3c43cf7ddd2b019b267e7b106fc445cc872a6d8
Eliot Sanford <eliot587@gmail.com>
2020-09-27 23:05:40 -0500

    Build a handler function plus an addNote function on submit

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({notes}) => {
-    return (
-      <>
-        <h1>Notes</h1>
-        <ul>
-          {notes.map(note => 
-            <Note key={note.id} note={note} />
-          )}
-        </ul>
-      </>
-    )
+const App = (props) => {
+const [notes, setNotes] = useState(props.notes)
+const [newNote, setNewNote] = useState('... a new note')
+
+const addNote = (event) => {
+  event.preventDefault()
+  const noteObject = {
+    content: newNote,
+    date: new Date().toISOString(),
+    important: Math.random() < 0.5,
+    id: notes.length + 1,
   }
+
+  setNotes(notes.concat(noteObject))
+  setNewNote('')
+}
+
+const handleNoteChange = (event) => {
+  console.log(event.target.value)
+  setNewNote(event.target.value)
+}
+
+return (
+  <>
+    <h1>Notes</h1>
+    <ul>
+      {notes.map(note => 
+        <Note key={note.id} note={note} />
+      )}
+    </ul>
+    <form onSubmit={addNote}>
+      <input 
+        value={newNote} 
+        onChange={handleNoteChange}/>
+      <button type="submit">save</button>
+    </form> 
+  </>
+)}
 
   export default App
