14edbab8f19eec957af5f22249515717548caa5d
Eliot Sanford <eliot587@gmail.com>
2020-09-28 10:09:15 -0500

    Add functionality to input new person's name via form closes #9

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import Name from './components/Name'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { date: "2020-09-28T14:29:52.146Z", id: 1 , name: 'Bob Loblaw'}
+  ]) 
+  const [ newName, setNewName ] = useState('... add a new person')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      date: new Date().toISOString(),
+      id: persons.length + 1,
+      name: newName,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <label>
+          name: <input
+                  value={newName}
+                  onChange={handlePersonNameChange} />
+        </label>
+        <section>
+          <button type="submit">add</button>
+        </section>
+      </form>
+      <h2>Numbers</h2>
+      <section>
+        <ul>
+          {persons.map(person => 
+            <Name key={person.id} name={person.name} />
+          )}
+        </ul>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Name.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Name = ({ name }) => (
+    <>
+        <li>{name}</li>
+    </>
+)
+
+export default Name
+
