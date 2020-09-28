ed75b6fdf2e9045d41346cd1def85320443ba372
Eliot Sanford <eliot587@gmail.com>
2020-09-27 23:27:03 -0500

    Add a working filter from reading for 2.b

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -4,6 +4,7 @@ import Note from './components/Note'
 const App = (props) => {
 const [notes, setNotes] = useState(props.notes)
 const [newNote, setNewNote] = useState('... a new note')
+const [showAll, setShowAll] = useState(true)
 
 const addNote = (event) => {
   event.preventDefault()
@@ -23,11 +24,20 @@ const handleNoteChange = (event) => {
   setNewNote(event.target.value)
 }
 
+const notesToShow = showAll 
+  ? notes 
+  : notes.filter(note => note.important === true)
+
 return (
   <>
     <h1>Notes</h1>
+    <section>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+      </button>
+    </section>
     <ul>
-      {notes.map(note => 
+      {notesToShow.map(note => 
         <Note key={note.id} note={note} />
       )}
     </ul>
