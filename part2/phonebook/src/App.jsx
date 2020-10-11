39cd2832d94aba4b051bbdbeaea6a0a8bba01074
Eliot Sanford <eliot587@gmail.com>
2020-10-11 18:42:16 -0500

    Refactor app to include newNumber state, input, and dispaly number

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import Name from './components/Name'
+import Item from './components/Item'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { date: "2020-09-28T14:29:52.146Z", id: 1 , name: 'Bob Loblaw'}
+    { date: "2020-09-28T14:29:52.146Z", id: 1 , name: 'Bob Loblaw', number: '555-555-555'}
   ]) 
   const [ newName, setNewName ] = useState('... add a new person')
+  const [ newNumber, setNewNumber ] = useState('... add a new number')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -13,27 +14,38 @@ const App = () => {
       date: new Date().toISOString(),
       id: persons.length + 1,
       name: newName,
+      number: newNumber,
     }
     if (persons.some(item => item.name === newName)) {
       return alert(`${newName} is already added to phonebook`)
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handlePersonNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+  const handlePersonNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
 
   return (
     <>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <label>
-          name: <input
-                  value={newName}
-                  onChange={handlePersonNameChange} />
+      <label style={{display: "block"}}>name: <input
+                type="text"
+                value={newName}
+                onChange={handlePersonNameChange} />
+        </label>
+        <label style={{display: "block"}}>number: <input
+                type="tel"
+                value={newNumber}
+                onChange={handlePersonNumberChange} />
         </label>
         <section>
           <button type="submit">add</button>
@@ -43,7 +55,7 @@ const App = () => {
       <section>
         <ul>
           {persons.map(person => 
-            <Name key={person.id} name={person.name} />
+            <Item key={person.id} name={person.name} number={person.number} />
           )}
         </ul>
       </section>
---
+++ part2/phonebook/src/components/Item.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Item = ({ name, number }) => (
+    <>
+        <li>{name} {number}</li>
+    </>
+)
+
+export default Item
+
