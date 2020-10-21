82fba491a17b46350d899458066449333a0b51da
Eliot Sanford <eliot587@gmail.com>
2020-10-21 17:27:54 -0500

    Build the useEffect with an eventHandler function inside it

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -8,17 +8,17 @@ const App = () => {
   const [newNote, setNewNote] = useState('... a new note')
   const [showAll, setShowAll] = useState(true)
   
-  const hook = () => {
+  useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
   
-  useEffect(hook, [])
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    }
+  
+    const promise = axios.get('http://localhost:3001/notes')
+    promise.then(eventHandler)
+  }, [])
 
 
 
