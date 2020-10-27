84be173793555c42f14e1b06f618a16db750660a
Eliot Sanford <eliot587@gmail.com>
2020-10-27 17:04:09 -0500

    Refactor into WIP services and make a functioning toggle function

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import axios from 'axios'
-
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -10,12 +10,12 @@ const App = () => {
   
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
+      noteService
+      .getAll()
       .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
   }, [])
 
 
@@ -45,9 +45,15 @@ const notesToShow = showAll
   ? notes 
   : notes.filter(note => note.important === true)
 
-const toggleImportanceOf = (id) => {
-  console.log(`importance of ${id} needs to be toggled`)
-}
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
 
 return (
   <>
@@ -66,7 +72,7 @@ return (
       <input 
         value={newNote} 
         onChange={handleNoteChange}/>
-      <button type="submit">save</button>
+  <button type="submit" style={{marginLeft: "1rem"}}>save</button>
     </form> 
   </>
 )}
