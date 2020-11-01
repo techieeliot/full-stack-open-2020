e50d847b5e61bef924143bb7dadf49be9bd16e90
Eliot Sanford <eliot587@gmail.com>
2020-11-01 14:53:52 -0600

    Refactor the notes app to set notes with initialNote and returnedNote

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -12,9 +12,9 @@ const App = () => {
     console.log('effect')
       noteService
       .getAll()
-      .then(response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
+      .then(initialNotes => {
+        console.log('promise fulfilled')
+        setNotes(initialNotes)
     })
   }, [])
 
@@ -28,10 +28,10 @@ const addNote = (event) => {
     important: Math.random() < 0.5,
   }
 
-  axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
+  noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
       setNewNote('')
     })
 }
@@ -50,9 +50,10 @@ const notesToShow = showAll
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
   
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id !== id ? note : response.data))
-    })
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))      })
   }
 
 return (
