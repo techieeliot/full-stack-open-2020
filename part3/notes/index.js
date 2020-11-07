5b2a5b516c50ccf8bce2e00546984a5d29e9284b
Eliot Sanford <eliot587@gmail.com>
2020-11-06 22:42:38 -0600

    Move docs into notes folder and install + use express

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {
@@ -21,10 +22,13 @@ let notes = [
     }
   ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end(JSON.stringify(notes))
-})
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
 
 const PORT = 3001
 app.listen(PORT)
