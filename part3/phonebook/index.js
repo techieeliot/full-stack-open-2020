9cad69a918c3203632cab7b67956cb7e0a6cc993
Eliot Sanford <eliot587@gmail.com>
2020-12-27 13:04:44 -0600

    Complete 3.4 to delete a single person

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -60,7 +60,7 @@ let persons = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello Phonebook!</h1><a href="http://localhost:6001/info">link</a>')
+    response.send('<h1>Hello Phonebook!</h1><a href="http://localhost:6001/info">INFO PAGE</a>')
   })
   
   app.get('/api/persons', (request, response) => {
@@ -69,7 +69,7 @@ app.get('/', (request, response) => {
 
 
   app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p><a href="http://localhost:6001/">HOME PAGE</a>`)
   })
 
   app.get('/api/persons/:id', (request, response) => {
@@ -80,6 +80,12 @@ app.get('/', (request, response) => {
     } else {
       response.status(404).end()
     }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
   })
 
 const PORT = 6001
