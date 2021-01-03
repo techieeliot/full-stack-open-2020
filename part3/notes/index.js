58c32dc97d8379bfd6bb1ec894c9850dae0a3fc5
Eliot Sanford <eliot587@gmail.com>
2021-01-03 14:49:59 -0600

    Add procfile and port

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -97,6 +97,7 @@ let notes = [
   
   app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
