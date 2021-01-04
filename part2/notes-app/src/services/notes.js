85c5ed569839c86deb4d6d54e8ee0210a4b6631b
Eliot Sanford <eliot587@gmail.com>
2021-01-04 17:08:19 -0600

    Add the heroku url for the backend

--- part2/notes-app/src/services/notes.js
+++ part2/notes-app/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://notes-techieeliot.herokuapp.com/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
