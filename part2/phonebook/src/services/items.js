b9c8b214b31363205431e5c850a60a41b3ff09ee
Eliot Sanford <eliot587@gmail.com>
2021-01-10 15:32:28 -0600

    Change personUrl for api

--- part2/phonebook/src/services/items.js
+++ part2/phonebook/src/services/items.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const personsUrl = 'http://localhost:3001/persons'
+const personsUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(personsUrl)
